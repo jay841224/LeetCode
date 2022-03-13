78fa98bd0eb2c8a87118c60447679f82f8a28a43
jay841224 <j1360fy6416nb@gmail.com>
2022-03-13 17:07:36 +0800

    【Title】新增7

---
+++ Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        long sum = 0;
+
+        while (x != 0) {
+            sum = sum * 10 + x % 10;
+            if (sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) {
+                return 0;
+            }
+            x = x / 10;
+        }
+        return (int)sum;
+        
+    }
+}
